ba4be7da756ad06e3a54ec43958934b5c46b1d22
valois pascal <pascal.valois@free.fr>
2019-10-11 00:46:49 +0200

    second commit

---
+++ kubepending.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import pint
+from kubernetes import client, config, watch
+from dumper import dump
+
+#Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+print("Listing pods in pending state :\n")
+
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for pod in ret.items:
+  if (pod.status.phase == 'Pending'):
+    print ("%-30s : %s," %(pod.metadata.name,pod.status.conditions[0].message))
