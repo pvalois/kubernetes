e5eb7f15fb757e741215a5afb796aa7018e0681e
valois pascal <pascal.valois@free.fr>
2023-06-28 19:18:55 +0200

    FIX: new version of pod status to date trace status evolution (for crash recovery timing and more)

--- pod_status.py
+++ pod_status.py
@@ -3,24 +3,34 @@
 import os, sys
 import pprint
 from kubernetes import client, config, watch
+from datetime import datetime
 
 try:
   name=sys.argv[1]
 except:
-  name=""
+  print ("please add a pod name or pod name part as argument")
+  exit (0)
 
 #Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 
 v1 = client.CoreV1Api()
 ret = v1.list_pod_for_all_namespaces(watch=False)
-for pod in ret.items:
-  if (name.lower() in pod.metadata.name.lower()):
-    print ("="*(len(pod.metadata.name)+20))
-    print (pod.metadata.name)
-    print ("="*(len(pod.metadata.name)+20))
-    print (pod.status.container_statuses)
-    if (pod.status.reason is not None): 
-      print (pod.status.reason,end="\n")
-    print ("")
+
+history={}
+
+while (True):
+  for pod in ret.items:
+    if (name.lower() in pod.metadata.name.lower()):
+      podname=pod.metadata.name;
+      status=pod.status.container_statuses[-1].state;
+
+      if (podname not in history):
+        history[podname]=""
+
+      if (history[podname]!=status):
+        print (podname,"status changed at",datetime.now(),"\n")
+        print (status)
+        print ()
+        history[podname]=status
 
