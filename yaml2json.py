408b44d7755cf08ecc4b652955be0f8318b5cec8
Pascal Valois <pvalois@email.com>
2025-08-23 11:40:17 +0200

    FIX: missing pieces

--- yaml2json.py
+++ yaml2json.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import yaml
 import json
-from collections import OrderedDict
 
 try:
   with open(sys.argv[1],"r") as f: document=f.read()
 except:
   if (len(sys.argv)>1): sys.exit(0)
-  print ("reading from stdin")
   document=sys.stdin
 
-print (json.dumps(yaml.load(document),indent=2))
+print (json.dumps(yaml.load(document, Loader=yaml.FullLoader),indent=2))
